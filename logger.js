cfec6032846b1db0636ccd9e858b81c012af4b44
Michael Shapiro <michaels@syncode.io>
2015-09-03 20:15:01 +0300

    Starting with something

---
+++ logger.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//     logger.js
+//     https://github.com/syncode/Bee-Logger-npm
+//     (c) 2015 Syncode Software Ltd.
+//     Bee Logger - Node Module Adapter may be freely distributed under the MIT license.
+
+var Logger = function() {
+
+};
+
+module.exports = Logger
