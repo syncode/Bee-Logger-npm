b7ca9bc42c20544557950183e5769c4c38b3400f
Michael Shapiro <michaels@syncode.io>
2015-09-03 20:59:30 +0300

    Started working on the adapter

--- logger.js
+++ logger.js
@@ -5,8 +5,49 @@
 //     (c) 2015 Syncode Software Ltd.
 //     Bee Logger - Node Module Adapter may be freely distributed under the MIT license.
 
+var http = require('http');
+
+var config = {
+	api: {
+		host: 'localhost',
+		port: '3000',
+		path: '/log'
+	}
+}
+
 var Logger = function() {
+
+	var data = {
+		key: '',
+		secret: ''
+	}
+
+	// Build the post string from an object
+	var post_data = JSON.stringify({
+		secret: data.secret,
+		no: 1,
+		message: 'test message'
+	});
+
+	// An object of options to indicate where to post to
+	var post_options = {
+		host: config.api.host,
+		port: config.api.port,
+		path: config.api.path + '/' + data.key,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': post_data.length
+		}
+	};
+
+	// Set up the request
+	var post_req = http.request(post_options);
+
+	// post the data
+	post_req.write(post_data);
+	post_req.end();
 
 };
 
-module.exports = Logger
+module.exports = Logger;
